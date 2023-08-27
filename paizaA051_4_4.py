93089908d8ef39cf1f1aa9d3a034b1c1137d4146
yseto <ysetottti@yahoo.co.jp>
2023-08-27 23:42:19 +0900

    first commit

---
+++ paizaA051_4_4.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+# import time
+# clock = time.time()
+#prepare Initial Conditional Parameter
+HW = [ int(s) for s in input().split(' ')]
+H = HW[0]
+W = HW[1]
+# print(HW)
+S = []
+for i in range(H):
+    S.append([ int(s) for s in input().split(' ')])
+#     S.append([ int(s) for s in input().split(' ')][0:1])
+# W=1
+# print(S)
+
+#Dynamic Program
+def dynamicProgram(array,w):
+    if len(array)==1:
+        return max(array[0])
+    maxArray = [ ]
+    for i in range(w):
+        if i==0:
+            maxArray.append(max(array[0][0:2]))
+            # print(i,array[0][0:2])
+        # elif i==w-1:
+        #     maxArray.append(max(array[0][w-2:w]))
+        #     # print(i,array[0][w-2:w])
+        else:
+            maxArray.append(max(array[0][i-1:i+2]))
+            # print(i,array[0][i-1:i+2])
+    # print(maxArray)
+        
+    array[1] = [x + y for (x, y) in zip(array[1],maxArray)]
+    array.pop(0)
+    # print(array)
+    return dynamicProgram(array,w)
+
+print(dynamicProgram(S,W))
